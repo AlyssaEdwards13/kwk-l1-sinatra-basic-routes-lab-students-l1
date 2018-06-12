8dc48acc6279e262fc276bc33eee3b98a9a4a814
AlyssaEdwards13 <edwardsalyssa83@gmail.com>
2018-06-12 21:28:54 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
-require_relative 'config/environment'
+require 'Sinatra'
 
 class App < Sinatra::Base
+  get '/' do 
+    "hello world!"
+  end 
+  
+  get '/name' do
+    "My name is alyssa"
+  end 
+    
+  get '/favorite song' do
+    "My favorite song is Poplar ST."
+  end 
+  
+  get '/home town' do
+    "My home town is Wallnut creek"
+  end 
 end
